bf0624fef786bea833b7433e03561f5380cb3b77
CesarHA <cesarhdezap@gmail.com>
2019-04-01 18:59:33 -0600

    Adición de la clase Student y Coordinador (simple) y recucción de lineas en el archivo DataBaseAccess.csproj

---
+++ SISS/BusinessLogic/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    class Student
+    {
+        private string Enrollment { get; set; }
+        private string Password { get; set; }
+        private string Name { get; set; }
+        private string Email { get; set; }
+        private string Career { get; set; }
+        private int Section { get; set; }
+        private int Block { get; set; }
+        private EStudentStatus StudentStatus { get; set; }
+
+
+        private enum EStudentStatus
+        {
+            OnHold,
+            Released,
+            Accepted,
+            Rejected,
+            Canceled
+        }
+    }
+}
