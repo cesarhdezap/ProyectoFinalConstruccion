85765f1adfaeb9ba73d5ca523b68ccd027c63909
CesarHA <cesarhdezap@gmail.com>
2019-04-12 08:30:36 -0500

    Cambio de idiona del acceso a base de datos a español, correccion de nombres de variables en calses del dominio, correccion de metodos de interfaces, creacion de los metodos de la clase Servicios de autenticacion y complemento en las funciones de AlumnoDAO y AsignacionDAO

---
+++ SISS/DataBaseAccess/AccesoADatos.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DataBaseAccess
+{
+	public class AccesoADatos
+	{
+		private static String CadenaDeConexion = System.Configuration.ConfigurationManager.ConnectionStrings["myConnection"].ConnectionString;
+		private const int PRIMERA_POSICION_EN_DATATABLE = 0;
+
+		public static DataTable ExecuteSelect(String Consulta, SqlParameter[] Parametros = null)
+		{
+			using (SqlConnection Conexion = new SqlConnection(CadenaDeConexion))
+			{
+				SqlCommand Comando = new SqlCommand(Consulta, Conexion);
+
+				if (Parametros != null)
+				{
+					Comando.Parameters.AddRange(Parametros);
+				}
+				
+				SqlDataAdapter DataAdapter = new SqlDataAdapter(Comando);
+				DataTable DataTable = new DataTable();
+
+				try
+				{
+					Conexion.Open();
+					Comando.ExecuteNonQuery();
+					DataSet DataSet = new DataSet();
+					DataAdapter.Fill(DataSet);
+					DataTable = DataSet.Tables[PRIMERA_POSICION_EN_DATATABLE];
+				}
+				catch (SqlException SqlException)
+				{
+					//TODO
+					Console.Write("Error - Coneccion.ejecutarConsultaSeleccionada - Consulta: " + Consulta + " \nExcepcion: " + SqlException.StackTrace.ToString());
+				}		
+				finally
+				{
+					CerrarConexion(Conexion);
+				}
+
+				return DataTable;
+			}
+		}
+
+		public static void CerrarConexion(SqlConnection Conexion)
+		{
+			if (Conexion != null)
+			{
+				if (Conexion.State == ConnectionState.Open)
+				{
+					Conexion.Close();
+				}
+			}
+		}
+			
+	
+		public static void EjecutarInsertarDentro(string Consulta, SqlParameter[] Parametros)
+		{
+			using (SqlConnection Conexion = new SqlConnection(CadenaDeConexion))
+			{
+				
+				SqlCommand Comando = new SqlCommand(Consulta, Conexion);
+				Comando.Parameters.AddRange(Parametros);
+				try
+				{
+					Conexion.Open();
+					Comando.ExecuteNonQuery();
+				}
+				catch (SqlException sqlException)
+				{
+					//TODO
+					Console.Write("Error - Coneccion.ejecutarConsultaSeleccionada - Consulta: " + Consulta + " \nExcepcion: " + sqlException.StackTrace.ToString());
+					throw sqlException;
+				}
+				finally
+				{
+					CerrarConexion(Conexion);
+				}
+			}
+		}
+	}
+}
