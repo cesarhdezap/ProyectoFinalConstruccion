f4543d5bd9eb64f6f92801c513ef8758ae2396d4
Alex <Patchinster@Gmail.com>
2019-06-10 11:02:26 -0500

    Reintegracion de metodos necesarios

--- SISS/LogicaDeNegocios/ClasesDominio/Alumno.cs
+++ SISS/LogicaDeNegocios/ClasesDominio/Alumno.cs
@@ -68,7 +68,7 @@ namespace LogicaDeNegocios
             alumnoDAO.ActualizarAlumnoPorMatricula(this.Matricula, this);
         }
 
-		internal void Asignar()
+		public void Asignar()
 		{
 			EstadoAlumno = EstadoAlumno.Asignado;
 			ActualizarRegistroDeAlumno();
