c44f0101d9c261885e699d7caccd807896936ac7
CESAR CHRISTOPHER HERNANDEZ APARICIO <zS17012947@estudiantes.uv.mx>
2019-06-19 15:40:11 -0500

    Se añadieron pruebas para acceso a datos Ejecutar insert into y select contemplando excepciones.

--- SISS/PruebasDeLogicaDeNegocios/PruebasDeAccesoADatos.cs
+++ SISS/PruebasDeLogicaDeNegocios/PruebasDeAccesoADatos.cs
@@ -1,78 +1,47 @@
 ﻿using System;
-using System.Text;
-using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using AccesoABaseDeDatos;
 using System.Data;
 using System.Data.SqlClient;
+using LogicaDeNegocios.Querys;
 
 namespace Pruebas
 {
-    /// <summary>
-    /// Descripción resumida de PruebasDeAccesoADatos
-    /// </summary>
     [TestClass]
     public class PruebasDeAccesoADatos
     {
-        public PruebasDeAccesoADatos()
-        {
-            //
-            // TODO: Agregar aquí la lógica del constructor
-            //
-        }
-
-        private TestContext testContextInstance;
-
-        /// <summary>
-        ///Obtiene o establece el contexto de las pruebas que proporciona
-        ///información y funcionalidad para la serie de pruebas actual.
-        ///</summary>
-        public TestContext TestContext
-        {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
-        }
-
-        #region Atributos de prueba adicionales
-        //
-        // Puede usar los siguientes atributos adicionales conforme escribe las pruebas:
-        //
-        // Use ClassInitialize para ejecutar el código antes de ejecutar la primera prueba en la clase
-        // [ClassInitialize()]
-        // public static void MyClassInitialize(TestContext testContext) { }
-        //
-        // Use ClassCleanup para ejecutar el código una vez ejecutadas todas las pruebas en una clase
-        // [ClassCleanup()]
-        // public static void MyClassCleanup() { }
-        //
-        // Usar TestInitialize para ejecutar el código antes de ejecutar cada prueba 
-        // [TestInitialize()]
-        // public void MyTestInitialize() { }
-        //
-        // Use TestCleanup para ejecutar el código una vez ejecutadas todas las pruebas
-        // [TestCleanup()]
-        // public void MyTestCleanup() { }
-        //
-        #endregion
-
         [DataTestMethod]
         [DataRow("INSERT INTO Alumnos VALUES ('s17012947', 'Cesar Hernandez', 'cesarhdez@gmail.com', '2282437357', 'LIS', '1', 'passwordsegura')")]
+        [DataRow("INSERT INTO Directores VALUES ('Juan Jimenez', 'juanj@correo.com', 'juanjpass')")]
 
-        public void ProbarEjecutarInsertInto_ConsultaCorrecta_RegresaNumeroDeFilasAfectadas(string input)
+        public void ProbarEjecutarInsertInto_ConsultaCorrecta_RegresaNumeroDeFilasAfectadas(string consulta)
         {
             int numeroDeFilasAfectadas = 0;
             const int UNA_FILA_AFECTADA = 1;
-            numeroDeFilasAfectadas = AccesoADatos.EjecutarInsertInto(input);
+            numeroDeFilasAfectadas = AccesoADatos.EjecutarInsertInto(consulta);
 
             Assert.AreEqual(UNA_FILA_AFECTADA, numeroDeFilasAfectadas);
         }
 
+        [DataTestMethod]
+        [DataRow("INSERT INTO Alumnos VALUES ('s17012947', 'Cesar Hernandez', 'cesarhdez@gmail.com', '2282437357', 'LIS', '1', 'passwordsegura')")]
+        public void ProbarEjecutarInsertInto_LlaveDuplicada_RegresaSqlException(string consulta)
+        {
+            bool excepcionTirada = false;
+            try
+            {
+                AccesoADatos.EjecutarInsertInto(consulta);
+            }
+            catch (SqlException ex) when (ex.Number == (int)CodigoDeErrorDeSqlException.InsercionFallidaPorLlavePrimariaDuplicada)
+            {
+                excepcionTirada = true;
+            }
+
+            if (!excepcionTirada)
+            {
+                throw new AssertFailedException("Se esperaba SqlException de error " + (int)CodigoDeErrorDeSqlException.InsercionFallidaPorLlavePrimariaDuplicada);
+            }
+        }
 
         [DataTestMethod]
         [DataRow("SELECT Matricula FROM Alumnos WHERE Matricula = @Matricula", "s17012947")]
@@ -94,14 +63,50 @@ namespace Pruebas
             Assert.AreEqual(matricula, matriculaActual);
         }
 
-        [DataTestMethod]
-        [DataRow("SELECT * FROM Alumnos",7)]
-        public void ProbarEjecutarSelect_AlumnosTodos_RegresaDatatable(string consulta, int numeroDeFilasEsperado)
+        [TestMethod]
+        public void ProbarEjecutarSelect_AlumnosTodos_RegresaDatatable()
         {
-            DataTable tablaAlumnosActual;
-            tablaAlumnosActual = AccesoADatos.EjecutarSelect(consulta);
-            int numeroDeFilas = tablaAlumnosActual.Rows.Count;
-            Assert.AreEqual(numeroDeFilasEsperado, numeroDeFilas);
+            AccesoADatos.EjecutarSelect("SELECT * FROM Alumnos");
+            Assert.IsTrue(true);
         }
+
+        [TestMethod]
+        public void ProbarEjecutarSelect_ColumnaInvalida_RegresaSqlException()
+        {
+            bool excepcionTirada = false;
+            try
+            {
+                AccesoADatos.EjecutarSelect("SELECT Nombres FROM Alumnos");
+            }
+            catch (SqlException ex) when (ex.Number == (int)CodigoDeErrorDeSqlException.ColumnaInvalida)
+            {
+                excepcionTirada = true;
+            }
+
+            if (!excepcionTirada)
+            {
+                throw new AssertFailedException("Se esperaba SqlException de error " + (int)CodigoDeErrorDeSqlException.ColumnaInvalida);
+            }
+        }
+
+        [TestMethod]
+        public void ProbarEjecutarSelect_ObjetoInvalido_RegresaSqlException()
+        {
+            bool excepcionTirada = false;
+            try
+            {
+                AccesoADatos.EjecutarSelect("SELECT Matricula FROM Alumno");
+            }
+            catch (SqlException ex) when (ex.Number == (int)CodigoDeErrorDeSqlException.ObjetoInvalido)
+            {
+                excepcionTirada = true;
+            }
+
+            if (!excepcionTirada)
+            {
+                throw new AssertFailedException("Se esperaba SqlException de error " + (int)CodigoDeErrorDeSqlException.ObjetoInvalido);
+            }
+        }
+
     }
 }
